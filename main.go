4cae655a829629b54bc04dad11d0f890f78d4520
Alex1997377 <lesha.shpatel@gmail.com>
2026-02-21 19:16:46 +0300

    feat: add checkHash function, IsValid function, CalculateHash function

--- main.go
+++ main.go
@@ -1,45 +1,87 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
 
 type Block struct {
-	Index     int
-	Timestamp time.Time
-	Data      string
-	Hash      string
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) CalculateHash() []byte {
+	headers := bytes.Join(
+		[][]byte{
+			b.PrevBlockHash,
+			b.Data,
+			[]byte(fmt.Sprintf("%d", b.Timestamp)),
+		},
+		[]byte{},
+	)
+	hash := sha256.Sum256(headers)
+	return hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+	}
+
+	block.Hash = block.CalculateHash()
+	return block
 }
 
 type Blockchain struct {
 	Blocks []*Block
 }
 
-func NewBlock(index int, data string) *Block {
-	return &Block{
-		Index:     index,
-		Timestamp: time.Now(),
-		Data:      data,
-		Hash:      fmt.Sprintf("hash-%d", index),
+func (bc *Blockchain) AddBlock(data string) {
+	var prevHash []byte
+
+	if len(bc.Blocks) > 0 {
+		prevHash = bc.Blocks[len(bc.Blocks)-1].Hash
+	} else {
+		prevHash = []byte{}
 	}
+
+	newBlock := NewBlock(data, prevHash)
+	bc.Blocks = append(bc.Blocks, newBlock)
 }
 
-func (bc *Blockchain) AddBlock(data string) {
-	index := len(bc.Blocks)
-	block := NewBlock(index, data)
-	bc.Blocks = append(bc.Blocks, block)
+func (bc *Blockchain) IsValid() bool {
+	for i := 1; i < len(bc.Blocks); i++ {
+		current := bc.Blocks[i]
+		previous := bc.Blocks[i-1]
+
+		if !bytes.Equal(current.Hash, current.CalculateHash()) {
+			return false
+		}
+
+		if !bytes.Equal(current.PrevBlockHash, previous.Hash) {
+			return false
+		}
+	}
+	return true
 }
 
 // Display displays all blocks in the blockchain
 func (bc *Blockchain) Display() {
-	fmt.Println("=== Blockchain ===")
-	for _, block := range bc.Blocks {
-		fmt.Printf("Block #%d\n", block.Index)
-		fmt.Printf("Timestamp: %s\n", block.Timestamp.Format(time.RFC3339))
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Println("---")
+	for i, block := range bc.Blocks {
+		fmt.Printf("--- Block ID: %d ---\n", i)
+		fmt.Printf("Timestamp: 	%d\n", block.Timestamp)
+		fmt.Printf("Data: 	   	%s\n", block.Data)
+		fmt.Printf("Prev Hash:  %s\n", hex.EncodeToString(block.PrevBlockHash))
+		fmt.Printf("Hash: 		%s\n", hex.EncodeToString(block.Hash))
+		fmt.Println("  --- ฿ ---  ")
 	}
 }
 
@@ -47,14 +89,15 @@ func main() {
 	fmt.Println("🚀 Hello Blockchain!")
 	fmt.Println("Creating your first blockchain...")
 
+	genesisBlock := NewBlock("Check working blockchain", []byte{})
 	// Create a new blockchain
-	blockchain := &Blockchain{}
+	blockchain := &Blockchain{Blocks: []*Block{genesisBlock}}
 
 	// Add some blocks
 	blockchain.AddBlock("Genesis Block - The beginning of our blockchain!")
 	blockchain.AddBlock("Second Block - Learning Go and blockchain!")
-	blockchain.AddBlock("Third Block - Building something amazing!")
 
+	fmt.Printf("Chain is valid: %v\n", blockchain.IsValid())
 	// Display the blockchain
 	blockchain.Display()
 
